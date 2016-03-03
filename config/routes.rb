8091640de5b1c11c3c360999fc0aab18b0704f8d
Daiki Nagaoka <Daiki@lneck.local>
2016-03-03 14:52:16 +0900

    ルーティングの変更("/" --> "top#index")

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   resources :items
-  resources :top, only: :index
+
+
+  root "top#index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
