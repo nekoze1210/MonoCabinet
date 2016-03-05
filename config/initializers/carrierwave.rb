8ed5dc4a6c8d3f040a80cdabac944e8e7d70cae1
Daiki Nagaoka <Daiki@lneck.local>
2016-03-05 19:41:03 +0900

    Amazon S3導入

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,18 +1,18 @@
 CarrierWave.configure do |config|
-    config.storage = :fog
+  config.storage = :fog
   config.fog_credentials = {
     provider: 'AWS',
     aws_access_key_id: ENV['ACCESS_KEY_ID'],
     aws_secret_access_key: ENV['SECRET_ACCESS_KEY'],
-    region: 'Oregon'
+    region: 'us-east-1'
   }
 
     case Rails.env
     when 'development'
-        config.fog_directory  = 'otoshimonno-image'
-        config.asset_host = 'https://s3.amazonaws.com/otoshimonno-image'
+        config.fog_directory  = 'otoshimono-image'
+        config.asset_host = 'https://s3.amazonaws.com/otoshimono-image'
     when 'production'
-        config.fog_directory  = 'ここにバケット名を入れます'
-        config.asset_host = 'https://s3.amazonaws.com/otoshimonno-image'
+        config.fog_directory  = 'otoshimono-image'
+        config.asset_host = 'https://s3.amazonaws.com/otoshimono-image'
     end
 end
