577bc6bb7420aac6ece1e76fdb6a78649081fccf
Daiki Nagaoka <Daiki@lneck.local>
2017-01-05 00:23:22 +0900

    adopt to heroku

--- app/services/tweet_posted_item_service.rb
+++ app/services/tweet_posted_item_service.rb
@@ -1,10 +1,14 @@
 class TweetPostedItemService
   def initialize
     @client = Twitter::REST::Client.new do |config|
-      config.consumer_key = Settings.twitter.consumer_key
-      config.consumer_secret = Settings.twitter.consumer_secret
-      config.access_token = Settings.twitter.token
-      config.access_token_secret = Settings.twitter.token_secret
+      # config.consumer_key = Settings.twitter.consumer_key
+      # config.consumer_secret = Settings.twitter.consumer_secret
+      # config.access_token = Settings.twitter.token
+      # config.access_token_secret = Settings.twitter.token_secret
+      config.consumer_key = ENV['TWITTER_CONSUMER_KEY']
+      config.consumer_secret = ENV['TWITTER_CONSUMER_SECRET']
+      config.access_token = ENV['TWITTER_TOKEN']
+      config.access_token_secret = ENV['TWITTER_TOKEN_SECRET']
     end
   end
 
--- config/initializers/rakuten.rb
+++ config/initializers/rakuten.rb
@@ -1,3 +1,4 @@
 RakutenWebService.configure do |config|
-  config.application_id = Settings.rakuten.application_id
+  # config.application_id = Settings.rakuten.application_id
+  config.application_id = ENV['RAKUTEN_APPLICATION_ID']
 end
