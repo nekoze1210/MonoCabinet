95657b001cdc5a3ecfa2e8dd1f9505f79fd0c212
Daiki Nagaoka <Daiki@lneck.local>
2017-01-04 19:20:51 +0900

    change heroku settings

--- config/initializers/config.rb
+++ config/initializers/config.rb
@@ -13,7 +13,11 @@ Config.setup do |config|
   # Define ENV variable prefix deciding which variables to load into config.
   #
   # config.env_prefix = 'Settings'
-
+  config.use_env = true
+  config.env_prefix = 'Settings'
+  config.env_separator = '__'
+  config.env_converter = :downcase
+  config.env_parse_values = true
   # What string to use as level separator for settings loaded from ENV variables. Default value of '.' works well
   # with Heroku, but you might want to change it for example for '__' to easy override settings from command line, where
   # using dots in variable names might not be allowed (eg. Bash).
