c22b8233999c12c16e3182ad8a575dd2fd2db1c3
Daiki Nagaoka <neko.z.n1210@gmail.com>
2020-01-01 11:47:16 +0900

    rubocop models/location

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,6 +1,7 @@
+# frozen_string_literal: true
+
 class Location < ActiveRecord::Base
-  require 'geocoder'
-  Geocoder.configure(:language  => :ja,  :units => :km, :lookup => :google) # 設定
+  Geocoder.configure(language: :ja, units: :km, lookup: :google) # 設定
   reverse_geocoded_by :latitude, :longitude
   after_validation :reverse_geocode
 
