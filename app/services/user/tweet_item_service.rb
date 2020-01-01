7970ec639df346910135a2e3a6a77039fcea6322
Daiki Nagaoka <neko.z.n1210@gmail.com>
2020-01-01 12:25:00 +0900

    refactor tweet service

--- app/services/user/tweet_item_service.rb
+++ app/services/user/tweet_item_service.rb
@@ -1,6 +1,22 @@
+# frozen_string_literal: true
+
 class User::TweetItemService
-  def initialize
-    @client = Twitter::REST::Client.new do |config|
+  def initialize(item)
+    @item = item
+  end
+
+  def execute
+    tweet = <<-MSG
+    MonoCabinet で #{@item.name} を失くさないように管理しています。
+    http://localhost:3000 #MonoCabinet
+    MSG
+    @client.update_with_media(tweet)
+  end
+
+  private
+
+  def client
+    Twitter::REST::Client.new do |config|
       if Rails.env == 'production'
         config.consumer_key = ENV['TWITTER_CONSUMER_KEY']
         config.consumer_secret = ENV['TWITTER_CONSUMER_SECRET']
@@ -13,14 +29,5 @@ class User::TweetItemService
         config.access_token_secret = Settings.twitter.token_secret
       end
     end
-  end
-
-  def post_to_twitter(item, image)
-    tweet = <<-MSG
-    MonoCabinet で #{item} を失くさないように管理しています。
-    http://localhost:3000 #MonoCabinet
-    MSG
-    media = open(image)
-    @client.update_with_media(tweet, media)
   end
 end
